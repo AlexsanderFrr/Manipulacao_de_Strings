0e8e6675c06c8e256718d5fbc32f3e17c830a384
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2023-07-06 11:42:58 -0300

    refazendo exercicio 2 padrão MVC

--- Exercicio2_RemoveDuplicate/src/main/java/com/mycompany/removeduplicate/RemoveDuplicate.java
+++ Exercicio2_RemoveDuplicate/src/main/java/com/mycompany/removeduplicate/RemoveDuplicate.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.removeduplicate;
 
+import controller.ControllerDuplicateRemove;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -15,33 +16,7 @@ import java.util.Set;
 public class RemoveDuplicate {
 
     public static void main(String[] args) {
-        // Obter a frase do usuário
-        System.out.print("Digite uma frase: ");
-        Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine();
-
-        // Remover caracteres duplicados
-        String outputString = removeDuplicateCharacters(inputString);
-
-        // Exibir o resultado
-        System.out.println("Frase sem caracteres duplicados: " + outputString);
-    }
-     public static String removeDuplicateCharacters(String inputString) {
-        // Usar um conjunto para armazenar os caracteres únicos em ordem
-        Set<Character> uniqueCharacters = new LinkedHashSet<>();
-
-        // Percorrer os caracteres da frase e adicioná-los ao conjunto
-        for (int i = 0; i < inputString.length(); i++) {
-            char currentChar = inputString.charAt(i);
-            uniqueCharacters.add(currentChar);
-        }
-
-        // Construir a string final sem os caracteres duplicados
-        StringBuilder outputBuilder = new StringBuilder();
-        for (Character character : uniqueCharacters) {
-            outputBuilder.append(character);
-        }
-
-        return outputBuilder.toString();
+          ControllerDuplicateRemove controller = new ControllerDuplicateRemove();
+        controller.removeDuplicateCharacters();
     }
 }
---
+++ Exercicio2_RemoveDuplicate/src/main/java/view/ViewDuplicateRemove.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alexs
+ */
+public class ViewDuplicateRemove {
+    
+    public String getInputString() {
+        System.out.print("Digite uma frase: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public void displayOutputString(String outputString) {
+        System.out.println("Frase sem caracteres duplicados: " + outputString);
+    }
+    
+}
